181bda3193b37387ab823b3fc9f90ad279887601
huuph <huphuc2@gmail.com>
2020-04-16 23:46:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/danghuuphuc_59131890_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.danghuuphuc_59131890_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
