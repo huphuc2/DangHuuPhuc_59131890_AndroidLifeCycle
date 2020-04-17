0b2f32fcadb7d596be6d687fd0958324d85f4019
huuph <huphuc2@gmail.com>
2020-04-17 10:18:14 +0700

    LifeCycle

--- app/src/main/java/com/example/danghuuphuc_59131890_lifecycle/MainActivity.java
+++ app/src/main/java/com/example/danghuuphuc_59131890_lifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.danghuuphuc_59131890_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle","onCreate Called");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart Called");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart Called");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","onResume Called");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle", "onPause Called");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle","onStop Called");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy Called");
     }
 }
